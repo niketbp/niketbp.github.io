51987ec272f35135fe9dd582c582da51b1d8b817
Niket Parikh <niketbp@umich.edu>
2017-01-04 00:54:58 -0500

    Added php script to handle the contact section

---
+++ assets/php/contact.php
@@ -0,0 +1,17 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "niketbp@umich.edu";
+    $from = $_POST['email']; //sender's email
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name. ", we will contact you shortly.";
+    }
+?>
