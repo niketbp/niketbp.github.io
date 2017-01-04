f3af83c68342eecf837b42dfa26f4b459140238d
Niket Parikh <niketbp@umich.edu>
2017-01-04 00:56:49 -0500

    Edited thank you message

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -6,12 +6,12 @@ if(isset($_POST['submit'])){
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message, " . $name . ".\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name. ", we will contact you shortly.";
+    echo "Thank you for message, " . $name. ". I will be sure to get back to you as soon as possible!";
     }
 ?>
